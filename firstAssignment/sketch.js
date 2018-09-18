ffc02448e1de6b0fbd45ac9b6e65b90119fe66d2
asir-ratnani <43014332+asir-ratnani@users.noreply.github.com>
2018-09-18 14:01:18 -0600

    DVD Bounce

---
+++ firstAssignment/sketch.js
@@ -0,0 +1,21 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(0)
+}
+
+function draw() {
+  ellipse (10,10,10,10);
+  fill(0,175,0);
+rect (windowWidth / 2,windowHeight - 100,150, 25);
+fill(200,0,0);
+ellipse(windowWidth / 2,windowHeight / 2,50);
+
+
+}
