a7ddca489c8c4e9c8bb176ad0c4144b0e40e07af
asir-ratnani <43014332+asir-ratnani@users.noreply.github.com>
2018-10-14 00:27:17 -0600

    State Variable Assignment - Oct 13
    Configured menu screen via state variables. Linked pong and ??? buttons to designated projects.
    
    To-do:
    - Fix bugs in pong
    - Create an AI version
    - Create multiplayer (time permitting)
    - Extras! ;)

--- stateVariable/pong.js
+++ stateVariable/pong.js
@@ -1,19 +1,3 @@
-// Interactive Scene Assignment
-// Asir Ratnani
-// September 24, 2018
-//
-// Extra for Experts:
-// Use of sound in the background for more of a game aesthetic.
-// As the ball hits the paddle, it changes colour to a random colour.
-// There was also a use of a button in order to reset the game once the ball hits the ground.
-// A game over is included once the ball hits the ground.
-//
-// Credits:
-// Music by Eric Matyas
-// www.soundimage.org
-
-
-//List of variables used in this game
 let xBall, yBall;
 let dxBall, dyBall;
 let xPaddle, yPaddle;
@@ -21,57 +5,8 @@ let paddleWidth, paddleHeight;
 let dxPaddle;
 let colour_1, colour_2, colour_3;
 let counter;
-let backgroundMusic;
 let button_1;
 let speed;
-
-function preload() {
-  // Adding a background sound to enhance game effects
-  backgroundMusic = loadSound("assets/Game-Menu_Looping.mp3");
-}
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-
-  // Playing the background music on a loop.
-  backgroundMusic.loop();
-
-  //  Set the variables for the ball
-  speed = 7;
-  xBall = random(0,width);
-  yBall = 5;
-  dxBall = speed;
-  dyBall = speed;
-
-  // Set the variables for the paddle
-  paddleWidth = 150;
-  paddleHeight = 25;
-  dxPaddle = 25;
-  xPaddle = width / 2 - paddleWidth / 2;
-  yPaddle = height - 75;
-
-  // Setting miscellaneous variables
-  colour_1 = random(255);
-  colour_2 = random(255);
-  colour_3 = random(255);
-  counter = 0;
-}
-
-function draw() {
-  background(0);
-  //Displays all the text on the screen.
-  textSize(20);
-  text("Score", 50, 100);
-  fill(0,255,0);
-  text(counter, 50, 125);
-  textSize(32);
-  text("Single Player Pong!", windowWidth / 2 - 100, 100);
-  textSize(20);
-  text("By: Asir Ratnani", windowWidth / 2 - 45, 125);
-  // Calls the functions in order to draw and move the paddle and ball.
-  paddle();
-  ball();
-
-}
 
 function setupPong() {
   background(0);
